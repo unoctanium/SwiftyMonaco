e443b101c3c8bbec44115f74046d7c43772bdb59
Odo Maletzki <odo@maletzki.net>
2024-02-24 06:32:32 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
         .library(
             name: "SwiftyMonaco",
             targets: ["SwiftyMonaco"]),
-        .library(
-            name: "SwiftyMonacoDynamic",
-            type: .dynamic,
-            targets: ["SwiftyMonaco"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
